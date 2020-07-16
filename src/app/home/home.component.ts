278df719de03cbd0da7d615a7eb8ec3b46ee5066
sunny <sunnyvales789@gmail.com>
2020-07-16 22:46:09 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,28 +11,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
-  Imgdata=[ "MH1001-min.jpg",
-    "MH1002-min.jpg",
-     "MH1003-min.jpg",
-     'MH1004-min.jpg',
-    'MH1005-min.jpg',
-    'MH1006-min.jpg',
-     'MH1007-min.jpg',
-    'MH1008-min.jpg',
-   'MH1009-min.jpg',
-    'MH1010-min.jpg',
-     'MH1011-min.jpg',
-    'MH1012-min.jpg',
-     'MH1013-min.jpg',
-     'MH1014-min.jpg',
-    'MH1015-min.jpg',
-    'MH1016-min.jpg'];
-    Imgdata2=[ 'MH2001 Ant.-min.jpg',
-      'Mh2001 SS-min.jpg',
-      'MH2002 Ant.-min.jpg',
-       'MH2003 BS-min.jpg', 
-       'MH2003 SS-min.jpg',
-       'MH2004 Ant.-min.jpg'];
+  Imgdata=["portfolio-2.jpg","portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg",
+  "portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg"
+,"portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg","portfolio-1.jpg"];
+
   showimg(j){
     var modal = document.getElementById('myModal');
     var modalImg = <HTMLImageElement>document.getElementById("img01");
